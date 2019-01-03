37f2cf3f61332c41af5a54cd6a07c1415bbbf477
alarcon <miguel.alarcon.ortiz@gmail.com>
2019-01-04 00:43:09 +0100

    check the directory and extract info from command diskutil info

--- mount_nfts.sh
+++ mount_nfts.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-echo Hola mundo
+#move to /Volumes and read directories.
+DIR_VOL  = "/Volumes/"
+DIR_LIST ="$(ls $DIR_VOL)"
+UUID_HD  =""
+MOUNT_HD =""
+PATH_DEV =""
+
+for i in $DIR_LIST; do
+	#compare with normal dirs like Macintosh HD Preboot
+	if [ $i != "Macintosh" ] && [ $i != "HD" ] && [ $i != "Preboot" ]; then
+		#to get UUID from your HD drive
+		DISK_INFO=echo $(sudo diskutil info $i)
+		if [ -z "DISK_INFO" ]; then
+			echo Hard Drive not found
+			echo "$(exit -1)"
+		else #DISK_INFO with value so time to proccess info
+			
+		fi		
+	fi
+done
+
+#add line to get permisions
+#echo UUID=uuid_HD none ntfs rw>> /etc/fstab
+
